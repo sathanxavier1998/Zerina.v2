801b99f2e8f39b9612ef9643d8b00683848f73bf
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2023-08-29 01:13:54 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,3 +1,4 @@
+@TeamHMT
 import os
 import logging
 import random
@@ -53,7 +54,7 @@ async def start(client, message):
                     InlineKeyboardButton('✇ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✇', url=CHNL_LNK)
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("AgAD1wIAAmSzYFY") 
+        m=await message.reply_sticker("CAACAgUAAxkBAAEKfulk7OafLGAL1qfpkoaIJweaKrN2KwAC1wIAAmSzYFbVyILAmIYWvh4E") 
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
