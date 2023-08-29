13a808fee64f6996dc9667d039682174c21f2ca1
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2023-08-29 11:21:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 PICS = (environ.get('PICS', 'https://te.legra.ph/file/f83017d890b1b692a673a.jpg https://te.legra.ph/file/72602fb1f87727dde571b.jpg https://te.legra.ph/file/e37fbea2190470aaf4bc1.jpg https://te.legra.ph/file/9f4f70db519f1bdbcb27b.jpg https://te.legra.ph/file/72d74f632a2317f21136b.jpg https://te.legra.ph/file/018e5893fdaabe0f1cef4.jpg')).split() #SAMPLE PIC
-NOR_IMG = environ.get("NOR_IMG", "https://te.legra.ph/file/72d74f632a2317f21136b.jpg")
+NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/15382eeff7d2f95d59d6f.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/b26637d70f3630a7e0fa1.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/314928baed9a9c277072f.jpg")
 
